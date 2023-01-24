f2d1391a2eb9c040b07dc1a333db07294c6d7c9b
MarDuo2022 <113748980+MarDuo2022@users.noreply.github.com>
2023-01-24 21:34:19 +0800

    used charts.js to plot yearly death counts for several causes of death

--- code/js/plot.js
+++ code/js/plot.js
@@ -1,26 +1,30 @@
 
-  var xyValues = [
-    {x:50, y:7},
-    {x:60, y:8},
-    {x:70, y:8},
-    {x:80, y:9},
-    {x:90, y:9},
-    {x:100, y:9},
-    {x:110, y:10},
-    {x:120, y:11},
-    {x:130, y:14},
-    {x:140, y:14},
-    {x:150, y:15}
-  ];
-  
-  new Chart("myChart", {
-    type: "scatter",
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'bar',
     data: {
+      labels: ["2014", "2015", "2016", "2017", "2018","2019"],
       datasets: [{
-        pointRadius: 4,
-        pointBackgroundColor: "rgba(0,0,255,1)",
-        data: xyValues
+        label: "Septicemia",
+        backgroundColor: "#FF9D76",
+        data: [38940,40773,40613,40922,40718,38431]
+      }, {
+        label: "Malignant Neoplasms",
+        backgroundColor: "#51EAEA",
+        data: [591700,595930,598038,599108,599274,599601]
+      }, {
+        label: "Diabetes Mellitus",
+        backgroundColor: "purple",
+        data:[76488,79535,80058,83564,84946,87647]
       }]
     },
-    // options:{...}
+    options: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Death counts by year for top causes of death'
+      }
+    }
   });
