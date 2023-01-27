399adf3020eda0a32f7d148a9b0c53e861f1bcf8
Brianna <oconnor.brianna@gmail.com>
2023-01-27 15:43:47 +0800

    add path to app.py. Get json data

--- code/js/app.js
+++ code/js/app.js
@@ -1,23 +1,14 @@
-// npm install csv-parse
-
-// const fs = require("fs");
-// const readline = require("readline");
-// const stream = fs.createReadStream("../../data/US-CDI_diabetes_prevalence.csv");
-// const rl = readline.createInterface({ input: stream });
-// let data = [];
-
-// rl.on("line", (row) => {
-//   data.push(row.split(","));
-// });
-
-// rl.on("close", () => {
-//   console.log(data);
-// });
-var data = require("../savedata_records.json");
-console.log(data.collection.length);
+// var data = require("code/savedata_records.json");
+// console.log(data.collection.length);
 
 // git pages url
 // import data from "../code/savedata_records.json" assert { type: "JSON" };
 // const data = JSON.parse("../../data/savedata_records.json");
-console.log("test");
-console.log(data[0]);
+//
+const path = "/code/savedata_records.json";
+
+const dataPromise = d3.json(path);
+
+console.log("Data Promise: ", dataPromise);
+
+console.log("test 2");
